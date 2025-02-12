b08a223fbc9c39bf624ee0f15a551ccdc796fb22
Bharath Vedartham <vedabharath12345@gmail.com>
2025-02-12 13:13:26 +0530

    update protos

--- codegen/src/main.rs
+++ codegen/src/main.rs
@@ -1,6 +1,3 @@
-const ROOT_DIR: &str = "./../";
-const OUT_DIR: &str = "./protobuf-core/src";
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let configured_builder = tonic_build::configure()
         .build_client(true)
@@ -12,8 +9,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("compiling protos!");
     configured_builder.compile_protos(
         &[
-            "./../proto/auctioneerapis/v1/payload_delivery.proto",
-            "./../proto/sequencerapis/v1/get_payload.proto",
+            "./../proto/auctioneerapis/v1/allocation_delivery.proto",
+            "./../proto/sequencerapis/v1/get_allocation.proto",
         ],
         &["./../proto/", "./../vendor/"],
     )?;
